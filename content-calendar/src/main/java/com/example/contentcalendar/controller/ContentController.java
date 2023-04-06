d35061e5fd5a8a39f8224e724a551538218ee430
earl-stephens <stephensearl@hotmail.com>
2023-04-06 17:07:58 -0600

    Add CORS annotation

--- content-calendar/src/main/java/com/example/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/com/example/contentcalendar/controller/ContentController.java
@@ -3,15 +3,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.contentcalendar.model.Content;
@@ -19,6 +11,7 @@ import com.example.contentcalendar.repository.ContentCollectionRepository;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
 	
 	private final ContentCollectionRepository repository;
